32859ec440500eb7ca458a5ca743287fb2d3a1a2
SABAHI Rim <rima.zbedawi@gmail.com>
2023-03-17 19:49:42 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/* * main - main function
+/**
+ *main - main function
  *
  * Return: always 0
  */
@@ -9,7 +10,7 @@ int main(void)
 {
 	int i;
 	char j;
-	
+
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
 	for (j = 'a' ; j <= 'f' ; j++)
